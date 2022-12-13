0fac1038e19aa22fdf4da8b83fd5a1f1c2ee17e8
Víctor Boix <victorboix@protonmail.com>
2022-12-13 12:01:48 +0100

    Update Llibreria_WiFi.ino

--- Llibreria_WiFi/Llibreria_WiFi.ino
+++ Llibreria_WiFi/Llibreria_WiFi.ino
@@ -10,8 +10,7 @@ void setup() {
   WiFi.mode(WIFI_STA);                     // Configura la placa en mode estació
   WiFi.begin("SSID", "password");          // Guarda les claus de la wifi a la memòria de la placa
   
-  Serial.print("Connectant a la xarxa ");  // Missatge de connexió al monitor
-  Serial.print(ssid);
+  Serial.print("Connectant a la xarxa");  // Missatge de connexió al monitor
   
   while (WiFi.status() != WL_CONNECTED) {  // Esperem a que es connecti
     delay(500);
